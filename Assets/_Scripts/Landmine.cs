22681800730645052a25d63051a974bc8b4b31ff
Brijesh Shir <shir.brijesh1992@live.com>
2017-10-02 10:38:53 -0400

    Final Game

---
+++ Assets/_Scripts/Landmine.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Landmine : MonoBehaviour {
+
+    public GameObject explosionPrefab;
+    public Transform respawnPoint;
+    // Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.tag == "Player")
+        {
+            explosionPrefab.SetActive(true);
+            other.gameObject.transform.position = respawnPoint.position;
+            
+        }
+    }
+
+   
+
+   
+}
+     
